e603c76c693ca625ac7105b7c9433d4b6d172e6d
Mark Zotov <zotov001@yandex.ru>
2023-01-15 15:21:37 +0300

    add media

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from django.conf.urls.static import static
 
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
@@ -17,6 +16,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
